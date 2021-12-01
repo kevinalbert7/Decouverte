1493da39120f845bc335d693baba2209742e63ed
kevinalbert7 <20ko@live.fr>
2021-12-01 16:00:53 +0100

    Fin exercice 1 React Native

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View, ScrollView, Pressable, Alert, ActivityIndicator } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.container}>
+      <View style={styles.textcontainer}>
+        <Text style={styles.text}>Lorem 20</Text>
+        <Text style={styles.text}>Lorem 20</Text>
+        <Text style={styles.text}>Lorem 20</Text>
+        <Image source={{uri:'https://www.konexio.eu/uploads/1/2/0/2/120245745/konexio-logo_1.png'}} style={{ width: 200, height: 100 }} />
+        <Image source={require('./assets/logo.png')} style={{ width: 200, height: 100 }} />
+        <Pressable onPress={() => {Alert.alert('title', 'Hello Alert')}} style={({ pressed }) => [{
+            backgroundColor: pressed
+              ? 'red'
+              : 'white'
+          }]}>
+          <Text>ALERT</Text>
+        </Pressable>
+        <ActivityIndicator size="large" />
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'blue',
+    paddingBottom:100,
   },
+  textcontainer: {
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    color: 'red',
+    alignItems: 'center',
+  },
+  text: {
+    marginTop: 80,
+    marginBottom: 80,
+    fontSize: 30,
+    fontWeight: 'bold',
+  }
 });
+
